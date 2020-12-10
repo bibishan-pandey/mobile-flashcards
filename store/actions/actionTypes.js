6fa55c711635f7282194574433605de8ac228666
Bibishan Pandey <bibishan.b.pandey@gmail.com>
2020-12-10 21:01:54 +0545

    feat: added action types for decks

---
+++ store/actions/actionTypes.js
@@ -0,0 +1,5 @@
+export const GET_DECKS = 'GET_DECKS';
+export const ADD_DECK = 'ADD_DECK';
+export const ADD_CARD_TO_DECK = 'ADD_CARD_TO_DECK';
+export const REMOVE_DECK = 'REMOVE_DECK';
+export const RESET_DECK = 'RESET_DECK';
