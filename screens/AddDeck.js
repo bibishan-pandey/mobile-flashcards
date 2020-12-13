fde5bc384b08482d80989b0fa18c3f5620189b12
Bibishan Pandey <bibishan.b.pandey@gmail.com>
2020-12-13 13:16:54 +0545

    feat: added add deck screen and decks screen

---
+++ screens/AddDeck.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {ScrollView, Text} from 'react-native';
+
+class AddDeck extends React.Component {
+  render() {
+    return (
+      <ScrollView>
+        <Text>Add Deck</Text>
+      </ScrollView>
+    );
+  }
+}
+
+export default AddDeck;
---
+++ screens/Decks.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {ScrollView, Text} from 'react-native';
+
+class Decks extends React.Component {
+  render() {
+    return (
+      <ScrollView>
+        <Text>Decks</Text>
+      </ScrollView>
+    );
+  }
+}
+
+export default Decks;
